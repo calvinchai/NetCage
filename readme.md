2db5dac1a71a291933f55d2a70b25b8ffa280d96
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-10-19 22:21:16 -0400

    Add support of loading from profile

--- readme.md
+++ readme.md
@@ -1,1 +1,92 @@
-# NetCage
+
+---
+# NetCage: Network Address Controller
+
+
+## TODO:
+- [ ] Add a `--verbose` option
+- [ ] Track all child processes
+- [ ] Support IPv6
+- [ ] Support Listening
+- [ ] Support domain names
+- [ ] Support port filter
+- [ ] Support wildcard
+
+**Version:** 1.0.0  
+**License:** MIT
+
+---
+
+## Description
+
+NetCage is a user-level network access control tool designed to provide network access restrictions for processes, particularly in environments where you don't have root access or are in incompatible containers. Unlike traditional methods such as iptables or firewalls, NetGuardian offers an alternative approach to ensure your processes remain within defined network parameters.
+
+---
+
+## Features
+
+- **User-level Control:** No root access required.
+- **Container-friendly:** Works seamlessly in a variety of container environments.
+- **Lightweight:** Minimal resource overhead.
+- **Custom Rules:** Define specific network access rules per process.
+
+---
+
+## Installation
+
+```bash
+git clone https://github.com/YourUsername/NetGuardian.git
+cd NetGuardian
+```
+
+---
+
+## Usage
+
+1. **Basic Usage:**
+
+    ```bash
+    netcage -r [RULE] -p [PROCESS_NAME/ID] 
+    ```
+
+2. **List Current Rules:**
+
+    ```bash
+    netcage --list
+    ```
+
+For more detailed usage instructions, please refer to the [User Guide](link-to-user-guide.md).
+
+---
+
+## Contributing
+
+We welcome contributions! Please check out our [CONTRIBUTING.md](link-to-contributing-guide.md) for guidelines.
+
+---
+
+## Support & Feedback
+
+For support or to provide feedback, please raise an issue on our [GitHub repository](https://github.com/YourUsername/NetGuardian).
+
+---
+
+## License
+
+NetGuardian is released under the [MIT License](link-to-license-file.md).
+
+---
+
+## Acknowledgments
+
+Special thanks to the community and everyone who contributed to making this project possible.
+
+---
+
+## Changelog
+
+- **1.0.0:** Initial release.
+
+For a detailed changelog, refer to the [CHANGELOG.md](link-to-changelog.md).
+
+---
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,86 @@
 extern crate nix;
 extern crate syscalls;
-
+extern crate clap;
+extern crate serde_yaml;
 use nix::sys::ptrace;
 use nix::sys::signal::Signal;
 use nix::sys::wait::waitpid;
 use nix::unistd::{execvp, fork};
-use nix::unistd::{ForkResult, Pid};
+use nix::unistd::{ForkResult};
 use std::ffi::{CStr, CString};
+use std::path::PathBuf;
+use std::fs;
 use syscalls::Sysno;
+use clap::{Parser};
+use serde::{Serialize, Deserialize};
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Args {
+    #[arg(short, long, value_name = "PROFILE", help = "Path to the profile to use")]
+    profile: Option<PathBuf>,
+
+    #[arg(name = "COMMAND", required = true)]
+    command: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Profile {
+    allowed_ips: Vec<String>,
+}
+
+
+
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <command> [args...]", args[0]);
-        return;
-    }
+    let args = Args::parse();
+    // println!("args: {:?}", args.command);
+    //load the profile
+    let profile = match args.profile {
+        Some(path) => {
+            let contents = fs::read_to_string(path).expect("Failed to read YAML file");
+            serde_yaml::from_str(&contents).expect("Failed to parse YAML file")
+        },
+        None => Profile {
+            allowed_ips: vec!["127.0.0.1".to_string()],
+        },
+    };
 
     match unsafe { fork() }.expect("Failed to fork") {
         ForkResult::Child => {
             ptrace::traceme().expect("Failed to set ptrace on child");
             std::thread::sleep(std::time::Duration::from_millis(100)); // Allow time for parent to start tracing
-
-            let command = CString::new(args[1].as_str()).unwrap();
-            let c_args: Vec<CString> = args[1..]
+            let c_args: Vec<CString> = args.command
                 .iter()
                 .map(|arg| CString::new(arg.as_str()).unwrap())
                 .collect();
             let c_args_ref: Vec<&CStr> = c_args.iter().map(AsRef::as_ref).collect();
+            let command = CString::new(args.command[0].as_str()).unwrap();
+            execvp(command.as_c_str(), &c_args_ref).unwrap_or_else(|_| {
+                eprintln!("Failed to execute {}", args.command[0]);
+                std::process::exit(1);
+            });
 
-            let _ = execvp(&command, &c_args_ref);
+
         }
         ForkResult::Parent { child } => {
-            trace_child(child);
+            std::thread::sleep(std::time::Duration::from_millis(100));
+
+            trace_child(child, profile);
         }
     }
 }
 
-fn trace_child(child: nix::unistd::Pid) {
+fn trace_child(child: nix::unistd::Pid, profile: Profile) {
     let mut in_syscall = false;
     loop {
         let status = waitpid(child, None).expect("Failed to waitpid");
-
+        // println!("status: {:?}", status);
+        // ptrace::setoptions(
+        //     child,
+        //     ptrace::Options::PTRACE_O_TRACEFORK |
+        //         ptrace::Options::PTRACE_O_TRACEVFORK |
+        //         ptrace::Options::PTRACE_O_TRACECLONE,
+        // ).expect("setoptions failed.");
         if let nix::sys::wait::WaitStatus::Stopped(_, Signal::SIGTRAP) = status {
             if in_syscall {
                 in_syscall = false;
@@ -60,7 +100,7 @@ fn trace_child(child: nix::unistd::Pid) {
                                 child,
                                 (sockaddr_ptr as usize + i * std::mem::size_of::<u64>()) as *mut _,
                             )
-                                .expect("Failed to read data")
+                                .expect("Failed to read data");
                         sockaddr_data
                             [i * std::mem::size_of::<u64>()..(i + 1) * std::mem::size_of::<u64>()]
                             .copy_from_slice(&data.to_ne_bytes());
@@ -71,12 +111,19 @@ fn trace_child(child: nix::unistd::Pid) {
                     let addr = unsafe { std::mem::transmute::<_, [u8; 4]>(sockaddr_in.sin_addr) };
                     let port = sockaddr_in.sin_port.to_be();
                     // block if the address is not localhost
-                    if addr != [127, 0, 0, 1] {
+                    let addr_str = format!("{}.{}.{}.{}", addr[0], addr[1], addr[2], addr[3]);
+                    if !profile.allowed_ips.contains(&addr_str)  {
                         println!("block connect to {:?}:{}", addr, port);
                         let mut regs = ptrace::getregs(child).expect("Failed to getregs");
                         regs.rax = -1i64 as u64;
                         ptrace::setregs(child, regs).expect("Failed to setregs");
                     }
+                    // if addr != [127, 0, 0, 1] {
+                    //     println!("block connect to {:?}:{}", addr, port);
+                    //     let mut regs = ptrace::getregs(child).expect("Failed to getregs");
+                    //     regs.rax = -1i64 as u64;
+                    //     ptrace::setregs(child, regs).expect("Failed to setregs");
+                    // }
                 }
 
                 in_syscall = true;
